6570e769511f0cb963bdd0ce7774e2fc7fe88df7
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2017-11-26 00:39:19 +0530

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,16 @@
+/*!
+ * dyCodeHighlighter
+ *
+ * Author: Yusuf Shakeel
+ * https://github.com/yusufshakeel
+ *
+ * GitHub Link: https://github.com/yusufshakeel/dyCodeHighlighter
+ *
+ * MIT license
+ * Copyright (c) 2017 Yusuf Shakeel
+ *
+ * Date: 2015-01-09 Friday
+ */
 module.exports = function(grunt) {
 
     // project configurations

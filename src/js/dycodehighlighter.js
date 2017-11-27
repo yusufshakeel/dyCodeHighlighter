ca12a037292a7a95b7957853b53c16929d4816a8
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2017-11-27 20:25:28 +0530

    header logic added

--- src/js/dycodehighlighter.js
+++ src/js/dycodehighlighter.js
@@ -15,25 +15,94 @@
 
     "use strict";
 
-    // variables
     var
+        // variables
         dyCodeHighlighter = {},
-        document = global.document;
+        document = global.document,
 
-    // constants
-    var
+        // constants
         DY_CODEHIGHLIGHTER_CLASS = "dyCodeHighlighter",
         DY_CODEHIGHLIGHTER_CLASS_CODE_LINE = "dyCodeHighlighter-line",
         DY_CODEHIGHLIGHTER_CLASS_CODE_LINE_HIGHLIGHT = "highlight",
+        DY_CODEHIGHLIGHTER_CLASS_CONTAINER = "dyCodeHighlighter-container",
+        DY_CODEHIGHLIGHTER_CLASS_HEADER = "dyCodeHighlighter-header",
         DY_CODEHIGHLIGHTER_CLASS_LINE_NUMBER_ROWS = "dyCodeHighlighter-line-numbers-rows",
         DY_CODEHIGHLIGHTER_DATA_ATTRIBUTE_HIGHLIGHT = "data-dyCodeHighlighter-highlight",
-        DY_CODEHIGHLIGHTER_DATA_ATTRIBUTE_LINE_START = "data-dyCodeHighlighter-line-start";
+        DY_CODEHIGHLIGHTER_DATA_ATTRIBUTE_LINE_START = "data-dyCodeHighlighter-line-start",
+        DY_CODEHIGHLIGHTER_DATA_ATTRIBUTE_HEADER = "data-dyCodeHighlighter-header";
 
     /**
      * This function will check if selected element has a given class.
+     *
+     * @param el
+     * @param val
+     * @returns {boolean}
      */
     function hasClass(el, val) {
         return el.className.split(" ").indexOf(val) >= 0;
+    }
+
+    /**
+     * This will wrap the element in a wrapper.
+     *
+     * @param el
+     * @param wrapper
+     */
+    function wrap(el, wrap) {
+        el.parentNode.insertBefore(wrap, el);
+        wrap.appendChild(el);
+    }
+
+    /**
+     * This will copy the default into source.
+     *
+     * @param source
+     * @param defaults
+     * @returns {object}
+     */
+    function extendSource(source, defaults) {
+        var property;
+        for (property in defaults) {
+            if (source.hasOwnProperty(property) === false) {
+                source[property] = defaults[property];
+            }
+        }
+        return source;
+    }
+
+    /**
+     * This function will add header.
+     *
+     * @param containerEl
+     * @param {object} option
+     */
+    function addHeader(containerEl, option) {
+        //variables
+        var
+            header,
+            html = "";
+
+        // create a div element for the header content
+        header = document.createElement("div");
+
+        // add class to the header div
+        header.className = DY_CODEHIGHLIGHTER_CLASS_HEADER;
+
+        // if showing lines
+        if (option.lines === 'show') {
+            html += "<span>" + option.totalLines + " Lines</span>";
+        }
+
+        // if showing lang
+        if (option.lang.length > 0) {
+            html += "<span>" + option.lang + "</span>";
+        }
+
+        // add the html to the header
+        header.innerHTML = html;
+
+        // prepend the header inside the container
+        containerEl.prepend(header);
     }
 
     /**
@@ -111,11 +180,22 @@
 
             // variables
             var
-                // option
+                // container element that will enclose the elem element
+                containerEl = document.createElement('div'),
+
+                // default header settings for the selected elem element
+                defaultHeader = {
+                    show: false,
+                    lines: 'hide',
+                    lang: ''
+                },
+
+                // default options for the selected elem element
                 option = {
                     showLineNumbers: false,
                     highlightLines: [],
-                    lineStart: 1
+                    lineStart: 1,
+                    header: defaultHeader
                 },
 
                 // get the first child <code> element of the selected elem
@@ -130,14 +210,31 @@
                 // line numbers row <span>
                 lineNumbersRowsSpan;
 
+            //---------- prepare the container element ------------
+            containerEl.className = DY_CODEHIGHLIGHTER_CLASS_CONTAINER;
+
+            //---------- prepare the selected elem element ----------
+
             // check if the user wants to show line numbers
             option.showLineNumbers = hasClass(elem, 'line-numbers');
 
             // check if the user wants to highlight any lines
-            option.highlightLines = elem.hasAttribute(DY_CODEHIGHLIGHTER_DATA_ATTRIBUTE_HIGHLIGHT) ? elem.getAttribute(DY_CODEHIGHLIGHTER_DATA_ATTRIBUTE_HIGHLIGHT).split(",") : [];
+            if (elem.hasAttribute(DY_CODEHIGHLIGHTER_DATA_ATTRIBUTE_HIGHLIGHT)) {
+                option.highlightLines = elem.getAttribute(DY_CODEHIGHLIGHTER_DATA_ATTRIBUTE_HIGHLIGHT).split(",");
+            }
 
-            // check if the user wants to start line number from other value.
-            option.lineStart = elem.hasAttribute(DY_CODEHIGHLIGHTER_DATA_ATTRIBUTE_LINE_START) ? Number(elem.getAttribute(DY_CODEHIGHLIGHTER_DATA_ATTRIBUTE_LINE_START)) : option.lineStart;
+            // check if the user wants to start line number from other value
+            if (elem.hasAttribute(DY_CODEHIGHLIGHTER_DATA_ATTRIBUTE_LINE_START)) {
+                option.lineStart = Number(elem.getAttribute(DY_CODEHIGHLIGHTER_DATA_ATTRIBUTE_LINE_START));
+            }
+
+            // check if the user wants to show header
+            if (elem.hasAttribute(DY_CODEHIGHLIGHTER_DATA_ATTRIBUTE_HEADER)) {
+                option.header = extendSource(JSON.parse(elem.getAttribute(DY_CODEHIGHLIGHTER_DATA_ATTRIBUTE_HEADER)), defaultHeader);
+                option.header.show = true;
+            }
+
+            global.console.log(option);
 
             // get formatted code from the code content
             formattedCodeObj = getFormattedCode(codeContent, option);
@@ -145,13 +242,28 @@
             // update the selected <code> element HTML
             codeEl.innerHTML = formattedCodeObj.formattedCode;
 
-            // if showing line numbers then adjust line number rows padding
+            // if showing line numbers then adjust line number rows
             if (option.showLineNumbers) {
                 lineNumbersRowsSpan = elem.querySelector("span." + DY_CODEHIGHLIGHTER_CLASS_LINE_NUMBER_ROWS);
                 lineNumbersRowsSpan.style.counterReset = "linenumber " + (option.lineStart - 1);
                 lineNumbersRowsSpan.style.left = -lineNumbersRowsSpan.offsetWidth - 20 + "px";
                 lineNumbersRowsSpan.style.width = lineNumbersRowsSpan.offsetWidth + 10 + "px";
                 elem.style.paddingLeft = lineNumbersRowsSpan.offsetWidth + 10 + "px";
+            }
+
+            //--------------- wrap the selected elem element inside the container element --------------
+            wrap(elem, containerEl);
+
+            // if showing header
+            if (option.header.show === true) {
+
+                // append totalLines to the header object if showing lines
+                if (option.header.lines === 'show') {
+                    option.header.totalLines = formattedCodeObj.totalLines;
+                }
+
+                // add header to the container element
+                addHeader(containerEl, option.header);
             }
 
         });
